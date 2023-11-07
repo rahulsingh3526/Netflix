67abd061e4e18ce1a6f9f84c501580973c8dcf80
Rahul_Singh <koderrahul@gmail.com>
2023-11-07 08:47:04 +0530

    feat: sign out the user

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,30 @@
+import { signOut, getSession } from "next-auth/react";
+import { NextPageContext } from "next";
+
+export async function getServerSideProps(context: NextPageContext) {
+  const session = await getSession(context);
+  console.log(session);
+  if (!session) {
+    return {
+      redirect: {
+        destination: "/auth",
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {},
+  };
+}
+
 export default function Home() {
-  return <h1 className="text-7xl text-green-500 ">NetFlix Clone</h1>;
+  return (
+    <>
+      <h1 className="text-7xl text-green-500 ">NetFlix Clone</h1>;
+      <button className="h-10 w-full bg-white" onClick={() => signOut()}>
+        LogOut!
+      </button>
+    </>
+  );
 }
